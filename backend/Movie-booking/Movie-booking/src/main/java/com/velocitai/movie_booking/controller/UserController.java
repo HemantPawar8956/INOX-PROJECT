91bed1c274eaecccd44064a823b9181133cf8b6e
anwithgowda <nikhil.a@testyantra.com>
2024-09-30 20:11:34 +0530

    hello

--- backend/Movie-booking/Movie-booking/src/main/java/com/velocitai/movie_booking/controller/UserController.java
+++ backend/Movie-booking/Movie-booking/src/main/java/com/velocitai/movie_booking/controller/UserController.java
@@ -1,16 +1,22 @@
 package com.velocitai.movie_booking.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.velocitai.movie_booking.model.User;
 import com.velocitai.movie_booking.service.imp.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 @RequestMapping("/users")
 @RestController
@@ -36,4 +42,15 @@ public class UserController {
 
         return ResponseEntity.ok(users);
     }
+    
+    @PostMapping("/image")
+    public ResponseEntity<?> saveimage(@RequestParam MultipartFile file , HttpSession session,int id) throws IOException {
+		return userService.saveImage(file,session,id);
+		
+	}
+    @GetMapping("/image")
+    public ResponseEntity<?> findimage(long id) throws IOException {
+		return userService.findImage(id);
+		
+	}
 }
