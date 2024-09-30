86400c140cf872e78b0fdd3d90994226edaa3f64
Hemant Pawar <hemantpawar26102@gmil.com>
2024-09-30 20:24:43 +0530

    second commit

--- frontend/INOX-project/src/components/Layout.jsx
+++ frontend/INOX-project/src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Layout = () => {
-  return <div className="text-red-700">Hello guys, lets start the project !........</div>;
+  return <div>Hello guys, lets start the project !........</div>;
 };
 
 export default Layout;
