ae9f2f35e80b3811b648bfc8537c3c048e19674f
@mantasha <95071953+mantasha-siddiqui@users.noreply.github.com>
2024-09-30 21:53:08 +0530

    new

--- frontend/INOX-project/src/components/Layout.jsx
+++ frontend/INOX-project/src/components/Layout.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Corousel1 from "./Corousel1";
 import Corousel2 from "./Corousel2";
+import Payment from "./Payment";
 
 const Layout = () => {
   return (
   <div>
    <Corousel1 />
    <Corousel2 />
+
+  
   </div>
   );
 };
