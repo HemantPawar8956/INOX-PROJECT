c391d100b6ca7084ab64d91b103dfa579743cb21
sebi bano <sebiraeen.700@gmail.com>
2024-10-01 11:25:23 +0530

    seebi

--- frontend/INOX-project/src/pages/LoginPage.jsx
+++ frontend/INOX-project/src/pages/LoginPage.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>
+    <div >
+    hello
       
     </div>
   )
--- frontend/INOX-project/src/routing/InoxRouting.jsx
+++ frontend/INOX-project/src/routing/InoxRouting.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./../components/Layout";
+import SeatLayout from "../components/SeatLayout";
 
 export let routingVar = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+  },
+  {
+    path: "/seatlayout",
+    element: <SeatLayout/>,
   },
 ]);
